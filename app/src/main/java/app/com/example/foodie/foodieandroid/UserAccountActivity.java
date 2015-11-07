a5f9745988b8bcd07401a2dc22057d7f23fc9a66
Jennifer Li <jenniferli0823@gmail.com>
2015-11-06 17:30:38 -0700

    Changed color theme to orange and lime

--- app/src/main/java/app/com/example/foodie/foodieandroid/UserAccountActivity.java
+++ app/src/main/java/app/com/example/foodie/foodieandroid/UserAccountActivity.java
@@ -80,17 +80,17 @@ public class UserAccountActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camara) {
+        if (id == R.id.nav_history) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_review) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_settings) {
 
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
+
+        } else if (id == R.id.nav_about) {
 
         }
 
