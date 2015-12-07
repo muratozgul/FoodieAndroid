8b28ff0240a0b9023ea0668f5aa7e4671c060a5f
murat <murat.ozgul@sv.cmu.edu>
2015-12-07 11:42:52 -0800

    global state & volley

---
+++ app/src/main/java/app/com/example/foodie/foodieandroid/Application/FoodieApp.java
@@ -0,0 +1,69 @@
+package app.com.example.foodie.foodieandroid.Application;
+
+import android.app.Application;
+import android.content.res.Configuration;
+import android.text.TextUtils;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by muratozgul on 06/12/15.
+ */
+public class FoodieApp extends Application {
+    public static final String TAG = "FoodieApp";
+    private static FoodieApp singleton;
+    private static RequestQueue mRequestQueue;
+
+
+    public static synchronized FoodieApp getInstance(){
+        return singleton;
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        singleton = this;
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req, String tag) {
+        // tag is used for cancelling (a group of reqs) if needed
+        req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
+        getRequestQueue().add(req);
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        req.setTag(TAG);
+        getRequestQueue().add(req);
+    }
+
+    public void cancelPendingRequests(Object tag) {
+        if (mRequestQueue != null) {
+            mRequestQueue.cancelAll(tag);
+        }
+    }
+}
